a1a768e03f5ad1504f83942026242dd9fcc22cb1
Anish-dev <anishb2041@gmail.com>
2024-04-07 05:56:32 +0530

    created header , hero section and added authentication

---
+++ app/(routes)/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <LogoutLink>Logout</LogoutLink>
+    </div>
+  )
+}
+
+export default page
