96d535d8c354eafce069bf5c35c1e9c637c44feb
Anish <Anish@lamarr.tech>
2024-04-11 07:16:52 +0530

    created availability section, scheduled section for late

---
+++ app/(routes)/dashboard/scheduled-meeting/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ScheduleMeet = () => {
+  return (
+    <div className='p-1-'>
+    <h2 className='font-bold text-2xl'>Scheduled Meeting</h2>
+    <hr className='my-7'></hr>
+    <div>
+    <h3 className='text-2xl text-slate-500'>Under Construction 🚧👷</h3>
+    </div>
+    </div>
+  )
+}
+
+export default ScheduleMeet
